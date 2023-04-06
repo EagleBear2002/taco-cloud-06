a0664fd745a04993ba6eae7a85cd7c83adee2fc8
Qiuhuan Xiong <eaglebear@smail.nju.edu.cn>
2023-04-06 16:10:27 +0800

    reformat

--- taco-cloud/src/main/java/tacos/web/AdminController.java
+++ taco-cloud/src/main/java/tacos/web/AdminController.java
@@ -4,28 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import tacos.data.service.OrderAdminService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-  
-  private OrderAdminService adminService;
-
-  public AdminController(OrderAdminService adminService) {
-    this.adminService = adminService;
-  }
-  
-  @GetMapping
-  public String showAdminPage() {
-    return "admin";
-  }
-
-  @PostMapping("/deleteOrders")
-  public String deleteAllOrders() {
-    adminService.deleteAllOrders();
-    return "redirect:/admin";
-  }
-
+	
+	private OrderAdminService adminService;
+	
+	public AdminController(OrderAdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+	@GetMapping
+	public String showAdminPage() {
+		return "admin";
+	}
+	
+	@PostMapping("/deleteOrders")
+	public String deleteAllOrders() {
+		adminService.deleteAllOrders();
+		return "redirect:/admin";
+	}
+	
 }
